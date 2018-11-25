324fbd5d13499966b59519832947959c9399136b
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-11-24 19:20:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,155 @@
-const questions = [
-  {questionText: "Lightning never strikes in the same place twice", answer: false},
-  {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
-  {questionText: "Goldfish only have a memory of about three seconds", answer: false}
+// // questions object, containing sample questions
+// const questions = [{
+//     questionText: "Lightning never strikes in the same place twice",
+//     answer: false
+//   },
+//   {
+//     questionText: "Humans can distinguish between over one trillion different smells",
+//     answer: true
+//   },
+//   {
+//     questionText: "Goldfish only have a memory of about three seconds",
+//     answer: false
+//   }
+// ]
+
+// let question;
+
+// function askQuestion() {
+//   return questions[0];
+// }
+
+// // appendQuestion - appends the question to the "question-container" in the index.html file
+// const appendQuestion = function (question) {
+//   const questionContainer = document.querySelector(`.question-container`);
+//   questionContainer.innerHTML = question.questionText;
+// };
+
+// // askQuestionThen(time) - this assigns a global variable question to equal the first question i.e. question[0];
+// // it also returns a promise that is resolved after a specified amount of time (so that we can expire the question after 5 seconds); the amount of time to wait is provided as an argument to the function
+// const askQuestionThen = function (time) {
+//   return new Promise(function (resolve) {
+//     return question = questions[0];
+//     appendQuestion(question);
+//     setTimeout(function () {
+//       resolve();
+//     }, time);
+//   });
+// };
+
+
+// // removeQuestion - removes the question from the "question-container"; it returns a promise so that the function is "thennable", meaning we can do something after the question
+// const removeQuestion = function () {
+//   return new Promise(function (resolve) {
+//     const questionContainer = document.querySelector(`.question-container`);
+//     questionContainer.innerHTML = '';
+//     resolve();
+//   });
+// };
+
+// // askQuestionThenRemoveQuestion(time) - it appends the question to the "question-container"and after a specified amount of time removes the question;
+// // it takes an argument of "time" indicating the amount of time the question will be displayed
+
+// function askQuestionThenRemoveQuestion(time) {
+//   return askQuestionThen(time).then(removeQuestion)
+// }
+
+// // trueAndFalseButtons - it returns the collection of true and false buttons already provided in the index.html file
+
+// const trueAndFalseButtons = function () {
+//   return document.querySelector('.true-false-list').querySelectorAll('.btn');
+// };
+
+// // toggleTrueAndFalseButtons
+// // it adds the hide class if not on the buttons,
+// //  and removes the hide class if it is on the buttons
+
+// const toggleTrueAndFalseButtons = function () {
+//   trueAndFalseButtons().forEach(function (button) {
+//     button.classList.toggle('hide')
+//   });
+// };
+
+// // displayQuestionOnClick - it adds both the true and false buttons and the question
+// // for five seconds, and then hides both of the true and false questions and removes the text of the question
+
+// const displayQuestionOnClick = function () {
+//   let button = document.querySelector('a')
+
+//   return button.addEventListener('click', function () {
+//     return toggleTrueAndFalseButtons()
+//     askQuestionThenRemoveQuestion(5000)
+//   });
+// };
+
+
+// LEARN.CO VERSION
+
+const questions = [{
+    questionText: "Lightning never strikes in the same place twice",
+    answer: false
+  },
+  {
+    questionText: "Humans can distinguish between over one trillion different smells",
+    answer: true
+  },
+  {
+    questionText: "Goldfish only have a memory of about three seconds",
+    answer: false
+  }
 ]
 
 let question;
+
+function askQuestion() {
+  return questions[0]
+}
+
+function displayQuestionOnClick() {
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+
+function trueAndFalseButtons() {
+  return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function (btn) {
+    btn.classList.toggle("hide")
+  })
+}
+
+function checkQuestion(question, answer) {
+  question.questionAnswer == answer
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(question)
+    }, time)
+  })
+}
+
+function appendQuestion(question) {
+  let container = document.querySelector('.question-container')
+  container.innerHTML = question.questionText;
+}
+
+function removeQuestion() {
+  return new Promise(function () {
+    let container = document.querySelector('.question-container')
+    container.innerHTML = ''
+    toggleTrueAndFalseButtons()
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion)
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 describe('index', () => {
   describe('appendQuestion', () => {
     let question = questions[0]
-    it('appends the question to the question-container', function(){
+    it('appends the question to the question-container', function () {
       appendQuestion(question)
       expect(document.querySelector('.question-container').innerHTML).to.eq('Lightning never strikes in the same place twice')
     })
@@ -14,23 +14,23 @@ describe('index', () => {
 
     beforeEach(function () {
       questionContainer = document.querySelector('.question-container')
-     });
+    });
 
     it('should have question start off as undefined', () => {
       expect(question).to.eq(undefined)
     })
 
-    it("should assign a global variable question to equal a question", function() {
+    it("should assign a global variable question to equal a question", function () {
       askQuestionThen()
       expect(question.answer).to.be.a('boolean')
     })
 
-    it('should append the question', function(){
+    it('should append the question', function () {
       askQuestionThen()
       expect(document.querySelector('.question-container').innerHTML).to.eq('Lightning never strikes in the same place twice')
     })
 
-    it('should return a promise', function(){
+    it('should return a promise', function () {
       expect(askQuestionThen()).to.be.a('promise')
     })
   })
@@ -38,7 +38,7 @@ describe('index', () => {
   describe('removeQuestion', () => {
     let question = questions[0]
 
-    it('removes the question to the question-container', function(){
+    it('removes the question to the question-container', function () {
       appendQuestion(question)
       expect(document.querySelector('.question-container').innerHTML).to.eq('Lightning never strikes in the same place twice')
       removeQuestion()
@@ -46,32 +46,32 @@ describe('index', () => {
     })
   })
 
-  describe('askQuestionThenRemoveQuestion', function(){
+  describe('askQuestionThenRemoveQuestion', function () {
     let questionContainer;
     let removeQuestionSpy
     beforeEach(function () {
       questionContainer = document.querySelector('.question-container')
-     });
+    });
 
-    it('should append the question for 10 seconds and then remove the question when the promise resolves', function(){
+    it('should append the question for 10 seconds and then remove the question when the promise resolves', function () {
       let resolvingPromise = askQuestionThenRemoveQuestion(1000)
       expect(questionContainer.innerHTML).to.eq("Lightning never strikes in the same place twice")
-      resolvingPromise.then( (result) => {
-          expect(questionContainer.innerHTML).to.equal('');
+      resolvingPromise.then((result) => {
+        expect(questionContainer.innerHTML).to.equal('');
       })
     })
   })
 
-  describe('trueAndFalseButtons', function(){
-    it('selects and returns the true and false buttons in the index.html file', function(){
+  describe('trueAndFalseButtons', function () {
+    it('selects and returns the true and false buttons in the index.html file', function () {
       expect(trueAndFalseButtons().length).to.eq(2)
       expect(trueAndFalseButtons()[0].innerText).to.eq("TRUE")
       expect(trueAndFalseButtons()[1].innerText).to.eq("FALSE")
     })
   })
-  describe('toggleTrueAndFalseButtons', function(){
-    it('changes hidden true and false buttons to displayed', function(){
-      trueAndFalseButtons().forEach(function(btn){
+  describe('toggleTrueAndFalseButtons', function () {
+    it('changes hidden true and false buttons to displayed', function () {
+      trueAndFalseButtons().forEach(function (btn) {
         btn.classList.add('hide')
       })
       toggleTrueAndFalseButtons()
@@ -80,8 +80,8 @@ describe('index', () => {
       expect(Array.from(btns[1].classList)).to.not.include('hide')
     })
 
-    it('changes displayed true and false buttons to hidden', function(){
-      trueAndFalseButtons().forEach(function(btn){
+    it('changes displayed true and false buttons to hidden', function () {
+      trueAndFalseButtons().forEach(function (btn) {
         btn.classList.remove('hide')
       })
       toggleTrueAndFalseButtons()
@@ -91,9 +91,9 @@ describe('index', () => {
     })
   })
 
-  describe('displayQuestionOnClick', function(){
+  describe('displayQuestionOnClick', function () {
     let event;
-    beforeEach(function(){
+    beforeEach(function () {
       event = new MouseEvent('click', {
         'view': window,
         'bubbles': true,
@@ -101,7 +101,7 @@ describe('index', () => {
       });
     })
 
-    it('it displays the question for after a click event to the button', function(){
+    it('it displays the question for after a click event to the button', function () {
       displayQuestionOnClick()
       let btn = document.querySelector('.btn')
       btn.dispatchEvent(event)
@@ -109,13 +109,13 @@ describe('index', () => {
       expect(questionContainer.innerHTML).to.eq('Lightning never strikes in the same place twice')
     })
 
-    it('also displays the true and false buttons', function(){
+    it('also displays the true and false buttons', function () {
       displayQuestionOnClick()
       let btn = document.querySelector('.btn')
       btn.dispatchEvent(event)
       let questionContainer = document.querySelector('.question-container')
       let btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
-      setTimeout(function(){
+      setTimeout(function () {
         expect(Array.from(btns[0].classList)).to.not.include('hide')
         expect(Array.from(btns[1].classList)).to.not.include('hide')
       }, 500)
